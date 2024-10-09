4f8e23d4712bb0725c439abb643e5eda4ea03c75
GabrielaBressa <gabybressa85@gmail.com>
2024-10-09 11:42:26 -0300

    salvar

---
+++ javascript/aula03/exercicio.js
@@ -0,0 +1,18 @@
+//soma
+//let numero1 = 10;
+//let numero2 = 5;
+//let soma = numero1 + numero2;
+//console.log (`A soma de ${numero1} e ${numero2} é ${soma}`);
+
+//variavel string
+//let mensagem = "Olá, mundo!";
+//console.log(`conteudo da string: ${mensagem}`);
+
+//variavel booleana 
+//let ehVerdade = true
+//console.log(`O valor da variavel booleana: ${ehVerdade}`);
+
+//variavel numerica
+//let pi =3.145;
+//console.log(`o valor da variavel numerica pi é: ${pi}`); 
+
