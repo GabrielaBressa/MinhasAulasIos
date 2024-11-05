cbd54fc7403b7e4bfea9bba85b92b9e4afe7a3d4
GabrielaBressa <gabybressa85@gmail.com>
2024-11-05 09:40:00 -0300

    salvar

---
+++ javascript/aula11/class.js
@@ -0,0 +1,24 @@
+class animal {
+    constructor(especie, genero, cor, idade) {
+        this.especie = especie
+            this.genero = genero
+            this.cor = cor
+            this.idade = idade
+    }
+}
+
+let animal01 = new animal('mico leão', 'masculino', 'marrom', '10')
+
+console.log(animal01);
+
+  class Aluno {
+    constructor(nome, sobreNome, RA, matricula) {
+        this.nome = nome
+        this.sobreNome = sobreNome
+        this.RA = RA
+        this.matricula = matricula
+    }
+  }
+  let aluno01 = new animal('Gabriela', 'Bressa', '123', 'Dev.web')
+
+  console.log(aluno01);
---
+++ javascript/aula11/objetos.js
@@ -0,0 +1,40 @@
+let aluno01 = {
+    nome: 'Gabriela',
+    sobrenome:'Bressa',
+    RA: 123,
+    matricula: {
+        curso:'Dev.web',
+        alunoMatriculado: true
+    }
+}
+
+let aluno02 = {
+    nome: 'Caue',
+    sobrenome:'Herbert',
+    RA: 124,
+    matricula: {
+        curso:'Dev.web',
+        alunoMatriculado: true
+    }
+}
+
+let aluno03 = {
+    nome: 'Gustavo',
+    sobrenome:'Jeronimo',
+    RA: 153,
+    matricula: {
+        curso:'Dev.web',
+        alunoMatriculado: true
+    }
+}
+console.log(aluno01.nome);
+console.log(aluno01.RA);
+console.log(aluno01.matricula.curso);
+
+console.log(aluno02.nome);
+console.log(aluno02.RA);
+console.log(aluno02.matricula.curso);
+
+console.log(aluno03.nome);
+console.log(aluno03.RA);
+console.log(aluno03.matricula.curso);b
