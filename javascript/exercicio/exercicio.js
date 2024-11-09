d678c2b5b3cf872b07f3906fca93cb648ebfabe8
GabrielaBressa <gabybressa85@gmail.com>
2024-11-09 16:26:12 +0000

    salvar

---
+++ javascript/exercicio/exercicio.js
@@ -0,0 +1,29 @@
+//const jogos = ["The Legend of Zelda", "Minecraft", "Fortnite", "Super Mario", "Call of Duty"];
+
+//jogos.forEach(jogo => {
+    //console.log(jogo);
+ // });
+
+ //const jogos = ["The Legend of Zelda", "Minecraft", "Fortnite", "Super Mario", "Call of Duty"];
+
+ //jogos.forEach((jogo, indice, array) => {
+   // console.log(`Valor: ${jogo}, Índice: ${indice}, Array: [${array.join(', ')}]`);
+  //});
+
+  //const numbers = [1, 2, 3, 4, 5];
+
+ // const numerosPares = numbers.filter(numero => numero % 2 === 0);
+  //console.log(numerosPares); 
+
+  //const fruits = ['apple', 'banana', 'orange', 'kiwi'];
+
+ // fruits.forEach(fruit => {
+    //console.log(fruit);
+  //});
+
+  //let idades = [16, 48, 23, 22, 45, 8, 12];
+
+  //let idadesAdultas = idades.filter(idade => idade >= 18);
+ // idadesAdultas.forEach(idade => {
+    //console.log(`Sua idade é: ${idade}`);
+ // });
